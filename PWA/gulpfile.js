eef29e15206bf5195aaae3810fa6a154d53397a0
Ivan Penkov <ivanp@bbd.co.za>
2018-03-22 15:06:02 +0200

    sass

---
+++ PWA/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less"),
+    sass = require("gulp-sass");
+
+// other content removed
+
+gulp.task("sass", function () {
+    return gulp.src('/wwwroot/cscc/custom.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('/wwwroot/styles/'));
+});
